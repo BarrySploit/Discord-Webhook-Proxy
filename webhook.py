996d82bab0fc1eb09da92b44ce7dff5cdaa7ea97
BarrySploit <34023280+BarrySploit@users.noreply.github.com>
2025-01-28 12:19:19 -0700

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -3,15 +3,14 @@
 
 from flask import Flask, request, jsonify
 from werkzeug.datastructures import ImmutableMultiDict 
-import select
-import sys
-import re
 import logging
 import json
 import time
-import os
-import base64
 import requests
+
+elk_url = "https://10.5.20.4:5601/app/security/alerts"
+discord_webhook = "https://discord.com/api/webhooks/1326217223647399966/Y4IdrM84zvOo3oJ7tO6oFBW8RLj-CCSIR6K_XfGUvlDDTaY3dsj8FabAZeTPRutcxFMo"
+listen_port = 8080
 
 app = Flask(__name__)
 
@@ -26,18 +25,19 @@ def sendDiscordMessage(message):
         "embeds": [
             {
                 f"title": f"ELK Alert - {date}",
-                f"description": f"{status}\nView Recent Alerts: https://10.5.20.4:5601/app/security/alerts"
+                f"description": f"{status}\nView Recent Alerts: {elk_url}"
             }
         ]
     }
 
-    r = requests.post(url="https://discord.com/api/webhooks/1326217223647399966/Y4IdrM84zvOo3oJ7tO6oFBW8RLj-CCSIR6K_XfGUvlDDTaY3dsj8FabAZeTPRutcxFMo",json=data,headers=headers)
+    r = requests.post(url={discord_webhook},json=data,headers=headers)
     #print(r.status_code)
     if r.status_code != 204:
         print("ERROR SENDING DISCORD NOTIFICATION")
         print(r.status_code)
         print(type(r.status_code))
         print(r.text)
+        
 @app.route('/webhook', methods=['POST'])
 def post_endpoint():
     message = list(request.form.to_dict().keys())[0].strip('\"')
@@ -47,4 +47,4 @@ def post_endpoint():
 if __name__ == '__main__':
     log = logging.getLogger('werkzeug')
     log.setLevel(logging.ERROR)
-    app.run(host="0.0.0.0",port=8080)
+    app.run(host="0.0.0.0",port=listen_port)
